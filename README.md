d3053eb9b3b588ede60e39c32fd457a34b1b326f
Dynatrace-Jeroen-Hautekeete <jeroen.hautekeete@dynatrace.com>
2025-05-08 08:34:42 +0200

    Initial project share

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # DtSettingsDeprecation
+
+This utility will scan your Dynatrace tenant for settings objects that are scoped to topology entities.  
+When those entities haven't been observed over the past time, this tool will remove those configuration settings to keep your tenant clean.
+
+## Prerequisites
+
+Create Dynatrace token with scopes:  
+* read settings  
+* write settings  
+* read schemas  
+
+## Installation
+
+pip install -r requirements.txt
+
+## Run
+
+export DT_URL=https://{tenant-id}.live.dynatrace.com (or https://{mycluster}/e/{environment-uuid})  
+export DT_TOKEN=dt0c01.###.######  
+(optional, default=200) export SINCE={num-days}  
+  
+cd src  
+python deprecator.py  
---
+++ src/deprecator.py
@@ -0,0 +1,65 @@
+from datetime import datetime, timedelta, timezone
+import os
+from dynatrace import Dynatrace
+from dynatrace import TOO_MANY_REQUESTS_WAIT
+
+dt_url=os.getenv("DT_URL")
+dt_token=os.getenv("DT_TOKEN")
+since=int(os.getenv("SINCE","200"))
+
+dtapi = Dynatrace(dt_url,dt_token, retries=3, retry_delay_ms=1000, too_many_requests_strategy=TOO_MANY_REQUESTS_WAIT,headers={"X-Dynatrace-Script":"SettingsDeprecator"})
+
+suptypes = [ 
+    "HOST", 
+    "HOST_GROUP", 
+    "KUBERNETES_CLUSTER", 
+    "PROCESS_GROUP", 
+    "PROCESS_GROUP_INSTANCE",
+    "SERVICE",
+    "CLOUD_APPLICATION",
+    "CLOUD_APPLICATION_NAMESPACE",
+    "DISK"
+]
+unsuptypes = set()
+
+
+def deprecateSettings():
+    current_dateTime = datetime.now(timezone.utc)
+    deprecation=timedelta(days=since)
+    schemas = dtapi.settings.list_schemas()
+    for schema in schemas:
+        settingsobjects = dtapi.settings.list_objects(schema.schema_id,fields="objectId,value,scope")
+        if len(settingsobjects)>0:
+            print(f"Found schema: {schema.schema_id} : {len(settingsobjects)}")
+        
+        for sobject in settingsobjects:
+            if sobject.scope != "environment":
+                curtype = sobject.scope.split("-")[0]
+                removal = False
+                if curtype in suptypes:
+                    print(f"\tScope: {sobject.scope}")
+                    try:
+                        scopedentity = dtapi.entities.get(sobject.scope, fields="lastSeenTms")
+                        print(f"\t\tEntity: {scopedentity.display_name} : {scopedentity.last_seen}")
+                        
+                        if (scopedentity.last_seen + deprecation) < current_dateTime:
+                            print(f"\t\t\tTOO OLD - will be removed")
+                            removal = True
+                    except Exception as inst:
+                        print(type(inst))
+                        if "[404]" in inst.args[0]:
+                            print(f"\t\t\tNOT FOUND - will be removed")
+                            removal = True                        
+                else:
+                    unsuptypes.add(curtype)
+                if removal:
+                    dtapi.settings.delete_object(sobject.object_id)
+    
+    print(f"Unsupported types: {unsuptypes}")        
+        
+    
+
+if __name__ == "__main__":
+    deprecateSettings()
+
+
